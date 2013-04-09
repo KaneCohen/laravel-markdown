7fb6bf81ccffab28f8f91fc9ad2e5c0a4e766106
Kane Cohen <kanecohen@gmail.com>
2013-04-09 10:37:25 +0100

    Add facade + Service Provider

---
+++ src/Markdown/MarkdownServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+namespace Markdown;
+
+use Illuminate\Support\ServiceProvider;
+
+class MarkdownServiceProvider extends ServiceProvider
+{
+  /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+  protected $defer = true;
+
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+		$this->app['markdown'] = $this->app->share(function($app) {
+			return new MarkdownExtra();
+		});
+  }
+
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+  public function provides()
+  {
+    return array('markdown');
+  }
+}
+
