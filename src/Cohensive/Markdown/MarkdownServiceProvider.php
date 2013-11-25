6f87fbc9d5e042d3f72258cec73c044b0067241e
Kane Cohen <kanecohen@gmail.com>
2013-11-25 19:40:15 +0000

    Make deferred.

--- src/Cohensive/Markdown/MarkdownServiceProvider.php
+++ src/Cohensive/Markdown/MarkdownServiceProvider.php
@@ -6,6 +6,13 @@ use Illuminate\Support\ServiceProvider;
 class MarkdownServiceProvider extends ServiceProvider
 {
   /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+  protected $defer = true;
+
+  /**
    * Register the service provider.
    *
    * @return void
